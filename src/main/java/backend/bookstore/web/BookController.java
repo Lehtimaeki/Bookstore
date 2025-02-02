079db34dfd56e7ff585d1d0cbff9b521a20ecaf4
Lehtimaeki <anni.tuulevi.lehtimaki@gmail.com>
2025-02-02 17:33:36 +0200

    Booklist toimii

--- src/main/java/backend/bookstore/web/BookController.java
+++ src/main/java/backend/bookstore/web/BookController.java
@@ -1,16 +1,33 @@
 package backend.bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+
+import backend.bookstore.repository.BookRepository;
+
+
 
 @Controller
 public class BookController {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @GetMapping("/booklist")
+    public String showBookList(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "booklist";
+    }
+
     @RequestMapping("/index")
     @ResponseBody
     public String returnMessage() {
         return "This is index";
     }
-    
+
 }
     
