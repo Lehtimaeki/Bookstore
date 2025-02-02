9e5a07b406ebc8ed4d16ab1e6eaee8c4b79a8e72
Lehtimaeki <anni.tuulevi.lehtimaki@gmail.com>
2025-02-02 21:09:45 +0200

    Create lisätty

--- src/main/java/backend/bookstore/web/BookController.java
+++ src/main/java/backend/bookstore/web/BookController.java
@@ -3,10 +3,13 @@ package backend.bookstore.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.ui.Model;
 
+import backend.bookstore.model.Book;
 import backend.bookstore.repository.BookRepository;
 
 
@@ -22,6 +25,19 @@ public class BookController {
         model.addAttribute("books", bookRepository.findAll());
         return "booklist";
     }
+
+    @GetMapping("/addbook")
+    public String showAddBookForm(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    @PostMapping("/addbook")
+    public String addBook(@ModelAttribute Book book) {
+        bookRepository.save(book);
+        return "redirect:/booklist";
+    }
+
 
     @RequestMapping("/index")
     @ResponseBody
